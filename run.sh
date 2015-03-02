3dc5823370af39a2a6df1e0d0584a385931266e1
Steven Bickerton <steven.bickerton@gmail.com>
2015-03-02 16:22:02 +0900

    use 0.17m effective aperture

--- run.sh
+++ run.sh
@@ -43,7 +43,7 @@ LAMHI=7.0e-7 # used only to make diffraction templates
 # diameter of primary in metres
 APERTURES="0.10 0.15 0.20 0.30 0.50 0.70 1.00 1.50 2.00"
 #APERTURES="0.15 0.20 0.30 0.50"
-APERTURES="0.10"
+APERTURES="0.17"
 
 #FOVs="0.1 0.2 0.3 0.4"
 FOVs="0.4"
